99dbd954a5a46bfaaf1e4c215925c7fe90e585ad
SilvanaMenezes <silvanasales.menezes@gmail.com>
2024-05-29 10:29:14 -0300

    V.1.0 - Corrigindo funcionalidade do aplicativo

--- app/src/main/java/com/example/mqtt_android/MainActivity.java
+++ app/src/main/java/com/example/mqtt_android/MainActivity.java
@@ -9,11 +9,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.util.Log;
 
 import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
 
 import java.nio.charset.StandardCharsets;
+import java.net.IDN;
 
 public class MainActivity extends AppCompatActivity {
     private Mqtt5BlockingClient client;
@@ -21,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextMessage;
     private Button buttonSend;
     private static final String TOPIC = "topico01";
-
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,32 +51,47 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void setupMqttClient(){
-        String host = "c9aa94f9aaed4b7fa6f7289774b259b6.s1.eu.hivemq.cloud:8884/mqtt";
-        String username = "sivanadev.one@gmail.com";
+    private void setupMqttClient() {
+        String host = "c9aa94f9aaed4b7fa6f7289774b259b6.s1.eu.hivemq.cloud";
+        int port = 8883;
+        String username = "silvanadev.one@gmail.com";
         String password = "Sil@hivemq24";
 
-        client = Mqtt5Client.builder()
-                .serverHost(host)
-                .serverPort(8883)
-                .sslWithDefaultConfig()
-                .buildBlocking();
+        try {
+            String asciiHost = IDN.toASCII(host);
 
-        client.connectWith()
-                .simpleAuth()
-                .username(username)
-                .password(StandardCharsets.UTF_8.encode(password))
-                .applySimpleAuth()
-                .send();
+            client = Mqtt5Client.builder()
+                    .serverHost(asciiHost)
+                    .serverPort(port)
+                    .sslWithDefaultConfig()
+                    .buildBlocking();
 
-        client.subscribeWith()
-                .topicFilter(TOPIC)
-                .send();
+            Log.d(TAG, "Conectando ao servidor MQTT...");
+
+            client.connectWith()
+                    .simpleAuth()
+                    .username(username)
+                    .password(StandardCharsets.UTF_8.encode(password))
+                    .applySimpleAuth()
+                    .send();
+
+            Log.d(TAG, "Conexão estabelecida com sucesso.");
+
+            client.subscribeWith()
+                    .topicFilter(TOPIC)
+                    .send();
+
+            Log.d(TAG, "Inscrito no tópico: " + TOPIC);
+        } catch (IllegalArgumentException e) {
+            Log.e(TAG, "Hostname inválido: " + host, e);
+        } catch (Exception e) {
+            Log.e(TAG, "Erro ao conectar ou se inscrever no servidor MQTT", e);
+        }
     }
 
-    private void sendMessage(){
+    private void sendMessage() {
         String message = editTextMessage.getText().toString();
-        if(!message.isEmpty()){
+        if (!message.isEmpty()) {
             client.publishWith()
                     .topic(TOPIC)
                     .payload(StandardCharsets.UTF_8.encode(message))
@@ -84,9 +101,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
-        if(client != null){
+        if (client != null) {
             client.disconnect();
         }
     }
